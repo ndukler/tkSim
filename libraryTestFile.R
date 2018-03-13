46eb4789f519289f1db7e5384701010b90e3d3d4
Cole Wunderlich <cole.wunderlich+compbio@gmail.com>
2018-03-13 16:44:43 -0400

    Added baisc check for inferParameters

--- libraryTestFile.R
+++ libraryTestFile.R
@@ -1,6 +1,10 @@
 ##Package Test File
 library(tkSim)
-bkm=basicKineticModel(times=0:30,synthRate=1:10,degRate = rep(0.5,10))
+bkm=basicKineticModel(times=1:30,synthRate=1:10,degRate = rep(0.5,10))
 bkm=simulateData(bkm)
-bkm=simulateReads(bkm,3,1,errorModel=function(x){rep(2,length(x))}) #CLUGE
-head(bkm@data)
+bkm=simulateReads(bkm,expectedLibSize=10^6,replicates=1,errorModel=function(x){rep(10^6,length(x))}) #CLUGE
+bkm@data[10,] = rnbinom(30,mu=10,size=3)
+plot(bkm@data[10,])
+test=inferParameters(bkm)
+print(test)
+print(test$par[1]/test$par[2])
