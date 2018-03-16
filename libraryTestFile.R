a4049bfe69b3cf95bf63d0c4d00b2850f1a3e971
Cole Wunderlich <cole.wunderlich+compbio@gmail.com>
2018-03-16 15:50:18 -0400

    Updated library test file
    now contains the complete analysis routine

--- libraryTestFile.R
+++ libraryTestFile.R
@@ -2,9 +2,7 @@
 library(tkSim)
 bkm=basicKineticModel(times=1:30,synthRate=1:10,degRate = rep(0.5,10))
 bkm=simulateData(bkm)
-bkm=simulateReads(bkm,expectedLibSize=10^6,replicates=1,errorModel=function(x){rep(10^6,length(x))}) #CLUGE
-bkm@data[10,] = rnbinom(30,mu=10,size=3)
-plot(bkm@data[10,])
-test=inferParameters(bkm)
-print(test)
-print(test$par[1]/test$par[2])
+bkm=simulateReads(bkm,expectedLibSize=10^6,replicates=3,spikeInSizes = 200,errorModel=function(x){rep(10^6,length(x))}) #CLUGE
+bkm=inferParameters(bkm)
+plotParameterFit(bkm,geneIdx=1:3)
+# print(test$par[1]/test$par[2])
