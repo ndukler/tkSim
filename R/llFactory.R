67799f19f314d37ccd8a6b47ea44c7ae17b1465d
Cole Wunderlich <cole.wunderlich+compbio@gmail.com>
2018-03-30 16:36:46 -0400

    Updated log likelyhood to use gene-based dispersion model if specified

--- R/llFactory.R
+++ R/llFactory.R
@@ -1,7 +1,7 @@
 #produces a log lilkelyhood function for a given (set of) gene(s)
 #should be used for only one gene at a time for purposes of posterior estimation
 #' @include getAbund.R
-llFactory = function(geneIdx,object)
+llFactory = function(geneIdx,object,dispByGene)
 {
   obs = object@data[geneIdx,]
   time=object@expMetadata$time
@@ -9,10 +9,23 @@ llFactory = function(geneIdx,object)
   normFactors = object@normFactors
   dispersion = object@dispersionModel
 
-  return(function(params)
+  if(dispByGene)
   {
-    expMu = getAbund(params[1],params[2],time,initVal)*normFactors
-    logProb = dnbinom(obs,mu=expMu,size=dispersion(expMu), log = T)
-    return(sum(logProb,na.rm=T))
-  })
+    return(function(params)
+    {
+      expMu = getAbund(params[1],params[2],time,initVal)*normFactors
+      logProb = dnbinom(obs,mu=expMu,size=dispersion(geneIdx), log = T)
+      return(sum(logProb,na.rm=T))
+    })
+  }else
+  {
+    return(function(params)
+    {
+      expMu = getAbund(params[1],params[2],time,initVal)*normFactors
+      logProb = dnbinom(obs,mu=expMu,size=dispersion(expMu), log = T)
+      return(sum(logProb,na.rm=T))
+    })
+  }
+
+
 }
