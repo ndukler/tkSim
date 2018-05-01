0520ba0467dda9a68f786943237c85ec04760e38
Cole Wunderlich <cole.wunderlich+compbio@gmail.com>
2018-05-01 14:33:40 -0400

    Updated test file

--- libraryTestFile.R
+++ libraryTestFile.R
@@ -9,7 +9,7 @@ library(tkSim)
 bkm=basicKineticModel(times=0:30,synthRate=1:10,degRate = rep(0.5,10))
 bkm=simulateData(bkm)
 bkm=simulateReads(bkm,expectedLibSize=10^6,replicates=3,spikeInSizes = 200,dispersionModel=function(x){rep(10^3,length(x))},dispByGene=F) #CLUGE
-bkm = estimateDispersions(bkm)
+bkm = estimateDispersions(bkm) #update disperson model based on @data
 bkm=inferParameters(bkm)
 plotParameterFit(bkm,geneIdx=1:3)
 
